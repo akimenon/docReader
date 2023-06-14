82574362c8f8bd21ebb10a21c6d9264cdccedf00
Menon <akhmenon@3c22fbe8f40f.ant.amazon.com>
2023-06-13 22:14:46 -0700

    first commit

---
+++ utils.py
@@ -0,0 +1,15 @@
+# Import PyPDF4
+
+# convert PDF to text
+def textfrompdf(pdfDocument):
+    print(len(pdfDocument.pages))
+    # Create an empty list to store documents
+    text = ""
+
+    # Iterate over all pages in the PDF
+    for page in pdfDocument.pages:
+        # Extract text from each page
+        text += page.extract_text()
+
+    # Print the list of documents
+    return text
